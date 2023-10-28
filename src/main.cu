fd78125acf8fc4fd7db75d7535d6c3a324d85a81
Vidith Balasa <vidith@Vidiths-MacBook-Air.local>
2023-10-28 00:35:50 -0700

    Added quantize method

--- src/main.cu
+++ src/main.cu
@@ -4,9 +4,22 @@
 #include <cuda_runtime.h>
 
 int quantize(int a, int b = 0) {
-    return 0;
+    // RANGE(-7 ~ 7) -> (0 ~ 15)
+    if (a > 7 || a < -7 || b > 7 || b < -7) {
+        printf("ERROR: quantize out of range\n");
+        exit(1);
+    }
+
+    a = a + 7;
+    b = b + 7;
+    return (a << 4) + b;
 }
 
 int main() {
+    int a = 6;
+    int b = -7;
+    int c = quantize(a, b);
+    printf("c = %d\n", c);
+
     return 0;
 }
