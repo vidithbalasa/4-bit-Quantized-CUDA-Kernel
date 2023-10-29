e213c1f96ab605a28d9e68cc802b0f92c62d2bc1
Vidith Balasa <vidithbalasa@gmail.com>
2023-10-28 17:09:22 -0700

    Added CUDA

--- src/main.cu
+++ src/main.cu
@@ -1,42 +1,111 @@
 #include <stdio.h>
-#include <cstdlib>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 #define MAX_4BIT 7
 #define MIN_4BIT -7
 
-int8_t quantize(int8_t a, int8_t b, int8_t* error) {
+__host__ __device__ int8_t quantize(int8_t a, int8_t b) {
     /*
     quantizes a pair of values to fit two 4-bit signed integers into a single byte
     */
     
     if (a > MAX_4BIT || a < MIN_4BIT || b > MAX_4BIT || b < MIN_4BIT) {
         printf("ERROR: Input integers must fit in 4 bits\n");
-        *error = 1;
-        return 0;
+        return 0x80; // Set highest bit to indicate error
     }
 
-    // Signed Ints: 2^3 & 2^8 ( [0]000[0]000 )
-    int8_t a_val = (a >> sizeof(int)-1) & 128;
-    int8_t b_val = (b >> sizeof(int)-1) & 8;
+    // Mask for value of signed ints: 2^3 & 2^8 ( [0]000[0]000 )
+    int8_t a_val = a & 0x08;
+    int8_t b_val = (b >> 4) & 0x08;
 
-    a = abs(a);
-    b = abs(b);
+    // Masking for absolute value
+    int8_t a_mask = a >> 7;
+    a = (a ^ a_mask) - a_mask;
+    int8_t b_mask = b >> 7;
+    b = (b ^ b_mask) - b_mask;
 
-    // Transalte 4bit ints to left & right half of 8-bit int
+    // transalte 4bit ints to left & right half of 8-bit int
     a_val += (a & 7) * 16;
     b_val += b & 7;
 
     return int8_t(a_val + b_val);
 }
 
-void split_ints(int8_t c, int8_t* split) {
-    /*
-    splits a single byte into two "4-bit" signed integers
-    */
+// __global__ void quantize_kernel(int8_t* arr, int8_t* quantized, int8_t size) {
+//     int index = blockIdx.x * blockDim.x + threadIdx.x;
+//     if (index < size-1) {
+//         quantized[index/2] = quantize(arr[index], arr[index+1]);
+//     }
+//     if (size % 2 == 1 && index == size-1) {
+//         quantized[size/2] = quantize(arr[size-1], -0);
+//     }
+// }
 
-    // Split the byte into two 4-bit integers
+__global__ void quantize_kernel(int8_t* arr, int8_t* quantized, int8_t size) {
+    __shared__ int errorFlag;
+    if (threadIdx.x == 0) {
+        errorFlag = 0;
+    }
+    __syncthreads();
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index < size-1) {
+        int8_t q_value = quantize(arr[index], arr[index+1]);
+        if (q_value & 0x80) {
+            atomicAdd(&errorFlag, 1);
+        } else {
+            quantized[index/2] = q_value;
+        }
+    } else if (size % 2 == 1 && index == size-1) {
+        quantized[size/2] = quantize(arr[size-1], 0);
+    }
+
+    __syncthreads();
+
+    // Return if there's an error
+    if (threadIdx.x == 0 && errorFlag) {
+        printf("ERROR: Input integers must fit in 4 bits\n");
+    }
+}
+
+void quantize_array(int8_t* arr, int8_t* quantized, int8_t size, int8_t* error) {
+    int8_t* d_arr;
+    int8_t* d_quantized;
+
+    cudaMalloc((void**)&d_arr, size * sizeof(int8_t));
+    cudaMalloc((void**)&d_quantized, ((size+1)/2) * sizeof(int8_t));
+
+    cudaMemcpy(d_arr, arr, size * sizeof(int8_t), cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+    quantize_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_arr, d_quantized, size);
+
+    cudaMemcpy(quantized, d_quantized, ((size+1)/2) * sizeof(int8_t), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_arr);
+    cudaFree(d_quantized);
+}
+
+void quantize_array_cpu(int8_t* arr, int8_t* quantized, int8_t size, int8_t* error) {
+    int8_t index = 0;
+    for (int i = 0; i < size-1; i += 2) {
+        quantized[index] = quantize(arr[i], arr[i+1]);
+        if (*error == 1) { return; }
+        index += 1;
+    }
+    int8_t out_len = int(size / 2) + (size % 2);
+    if (size % 2 == 1) {
+        quantized[out_len-1] = quantize(arr[size-1], -0);
+    }
+}
+
+void split_ints(int8_t c, int8_t* split) {
+    /* splits a single byte into two "4-bit" signed integers */
+
+    // split byte
     int8_t a = c >> 4;
     int8_t b = c & 15;
 
@@ -50,19 +119,6 @@ void split_ints(int8_t c, int8_t* split) {
     // Save 4bit int with multiplier
     split[0] = (a & 7) * a_multiplier;
     split[1] = (b & 7) * b_multiplier;
-}
-
-void quantize_array(int8_t* arr, int8_t* quantized, int8_t size, int8_t* error) {
-    int8_t index = 0;
-    for (int i = 0; i < size-1; i += 2) {
-        quantized[index] = quantize(arr[i], arr[i+1], error);
-        if (*error == 1) { return; }
-        index += 1;
-    }
-    int8_t out_len = int(size / 2) + (size % 2);
-    if (size % 2 == 1) {
-        quantized[out_len-1] = quantize(arr[size-1], -0, error);
-    }
 }
 
 void unquantize_array(int8_t* arr, int8_t* unquantized, int8_t size) {
@@ -79,27 +135,46 @@ void unquantize_array(int8_t* arr, int8_t* unquantized, int8_t size) {
 int main() {
     int8_t error = 0;
     // Test Quantize
-    int8_t a_len = 7;
+    int a_len = 1000000;
     int8_t out_len = int(a_len / 2) + (a_len % 2);
 
-    // list of items within quantize range
-    int8_t arr[a_len] = {0,-1,2,3,4,5,6};
+    // create array of random ints between -7 and 7
+    int8_t arr[a_len];
+    for (int i = 0; i < a_len; i++) {
+        arr[i] = rand() % 15 - 7;
+    }
+    
     //  Allocate 3 bytes of memory on host for int8 arrray
     int8_t out_arr[out_len];
     quantize_array(arr, out_arr, a_len, &error);
 
-    printf("Quantized Array: ");
-    for (int i = 0; i < out_len; i++) {
-        printf("%d ", out_arr[i]);
+    // int8_t unquantized[a_len];
+    // unquantize_array(out_arr, unquantized, out_len);
+
+    // make sure there's no error
+    if (error) {
+        printf("Error found in main call");
     }
 
-    int8_t unquantized[a_len];
-    unquantize_array(out_arr, unquantized, out_len);
-    printf("Unquantized Array: ");
-    for (int i = 0; i < a_len; i++) {
-        printf("%d ", unquantized[i]);
-    }
-    printf("\n");
+    // // Make sure original array matches output array
+    // for (int i = 0; i < a_len; i++) {
+    //     if (arr[i] != unquantized[i]) {
+    //         printf("ERROR: Original array does not match unquantized array\n");
+    //         return 0;
+    //     }
+    // }
+
+    // printf("Quantized Array: ");
+    // for (int i = 0; i < out_len; i++) {
+    //     printf("%d ", out_arr[i]);
+    // }
+    // printf("\n");
+
+    // printf("Unquantized Array: ");
+    // for (int i = 0; i < a_len; i++) {
+    //     printf("%d ", unquantized[i]);
+    // }
+    // printf("\n");
 
     return 0;
 }
